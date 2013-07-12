414895ae706398f30a6b707afbe4908f8e37d6b9
Ubuntu <ubuntu@ip-172-31-17-211.us-west-2.compute.internal>
2013-07-12 21:00:49 +0000

    adding buffer

--- web.js
+++ web.js
@@ -4,15 +4,8 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- fs.readFileSync('index.html','utf8',function(err,data){
-  
-    if(err) throw err;
-    //  var outputbuffer = new Buffer(1026);
-     // outputbuffer.write(data,"utf-8");
-     // response.send(outputbuffer.toString("utf-8"));
-     console.log(data);
-});
-response.send("back to app get fun);
+
+  response.send('back to app get fun');
 });
 
 var port = process.env.PORT || 5000;
